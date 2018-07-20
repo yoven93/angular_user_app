7f9c5ac64247302425ab1a345a244ffcb413a45b
Ayassamy <krisnasamy.ayassamy@accenture.com>
2018-07-20 20:39:48 +0400

    Created dtos

---
+++ src/app/dtos/address.ts
@@ -0,0 +1,4 @@
+export interface Address {
+  city: string;
+  zipcode: string;
+}
---
+++ src/app/dtos/user.ts
@@ -0,0 +1,10 @@
+import { Post } from './post';
+import { Address } from './address';
+
+export interface User {
+  readonly id: number;
+  name?: string;
+  email?: string;
+  address: Address;
+  posts: Post[];
+}
